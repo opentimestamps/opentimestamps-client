41ad039398c85c3f25a8b3093a1f36a66488cce2
Peter Todd <pete@petertodd.org>
2012-10-14 04:56:31 -0400

    Remove jsonrpclib and replace with RESTful API

--- opentimestamps/rpc.py
+++ opentimestamps/rpc.py
@@ -9,29 +9,70 @@
 # modified, propagated, or distributed except according to the terms contained
 # in the LICENSE file.
 
-import jsonrpclib
+import json
+import urllib.error
+import urllib.request
+from urllib.parse import quote_plus,unquote_plus,urlencode
 
 from .serialization import json_serialize,json_deserialize
 
-class OtsServer(object):
-    """JsonRPC marshaller proxy
-
-    Quick-n-dirty
-    """
+class OtsServer:
+    """Interface to an OpenTimestamps Server"""
 
     # FIXME: We should be doing things like ensuring the server is the right
     # version here.
 
     def __init__(self,url):
         self.url = url
-        self.__server = jsonrpclib.Server(url)
 
     def __getattr__(self,name):
-        fn = getattr(self.__server,name)
 
-        def marshalled_fn(*args):
-            json_args = [json_serialize(arg) for arg in args]
-            json_response = fn(*json_args)
-            return json_deserialize(json_response)
+        fn = None
+        if name.startswith('get_'):
+            name = name[4:]
 
-        return marshalled_fn
+            def get_fn(*args,**kwargs):
+                kwargs = {k: quote_plus(json.dumps(json_serialize(v))) for k,v in kwargs.items()}
+                kwargs = urlencode(kwargs)
+                if kwargs:
+                    kwargs = '?' + kwargs
+
+                args = [quote_plus(json.dumps(json_serialize(arg))) for arg in args]
+                args = '/'.join(args)
+                if args:
+                    args = '/' + args
+
+                try:
+                    with urllib.request.urlopen(self.url + '/' + name + args + kwargs) as r:
+                        print(r.geturl())
+                        print(r.info())
+                        return json_deserialize(json.loads(str(r.read(),'utf8')))
+                except urllib.error.HTTPError as ex:
+                    print(str(ex.read(),'utf8'))
+                    raise ex
+            fn = get_fn
+        elif name.startswith('post_'):
+            name = name[5:]
+
+            def post_fn(*args,**kwargs):
+                kwargs = {k: json.dumps(json_serialize(v)) for k,v in kwargs.items()}
+                kwargs = bytes(urlencode(kwargs),'utf8')
+
+                args = [quote_plus(json.dumps(json_serialize(arg))) for arg in args]
+                args = '/'.join(args)
+                if args:
+                    args = '/' + args
+
+                try:
+                    with urllib.request.urlopen(self.url + '/' + name + args,data=kwargs) as r:
+                        print(r.geturl())
+                        print(r.info())
+                        return json_deserialize(json.loads(str(r.read(),'utf8')))
+                except urllib.error.HTTPError as ex:
+                    print(str(ex.read(),'utf8'))
+                    raise ex
+            fn = post_fn
+        else:
+            raise Exception("Method must start with either 'get_' or 'post_'")
+
+        return fn
