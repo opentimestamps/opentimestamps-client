cc8d8c4b007b7dc5bcdc2dd4b868574a2a98541f
Peter Todd <pete@petertodd.org>
2016-08-22 01:34:30 -0400

    Fix copyright notices

--- opentimestamps/core/serialize.py
+++ opentimestamps/core/serialize.py
@@ -1,13 +1,13 @@
-# Copyright (C) 2016 The python-opentimestamps developers
+# Copyright (C) 2016 The OpenTimestamps developers
 #
 # This file is part of python-opentimestamps.
 #
 # It is subject to the license terms in the LICENSE file found in the top-level
 # directory of this distribution.
 #
-# No part of python-bitcoinlib, including this file, may be copied, modified,
-# propagated, or distributed except according to the terms contained in the
-# LICENSE file.
+# No part of python-opentimestamps including this file, may be copied,
+# modified, propagated, or distributed except according to the terms contained
+# in the LICENSE file.
 
 import io
 
