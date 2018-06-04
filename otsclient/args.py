bb138759e83ebad66dd90752904f35b018ecdad7
LeoComandini <leonardocomandini@hotmail.it>
2018-06-04 15:35:11 +0200

    add prune command

--- otsclient/args.py
+++ otsclient/args.py
@@ -216,12 +216,32 @@ def parse_ots_args(raw_args):
     parser_info.add_argument('file', metavar='FILE', type=argparse.FileType('rb'),
                              help='Filename')
 
+    # ----- prune -----
+    parser_prune = subparsers.add_parser('prune', aliases=['p'],
+                                         help='Prune timestamp')
+
+    prune_verify_group = parser_prune.add_mutually_exclusive_group()
+    prune_verify_group.add_argument('--verify', dest='attestations_to_verify', metavar='NOTARYSPEC', action='append',
+                                    type=str, default=[],
+                                    help='Verify attestations from a specified notary. May be specified multiple times.'
+                                         ' Default btc.')
+    prune_verify_group.add_argument('--no-verify', dest='no_verify', action='store_true', default=False,
+                                    help='Do not verify any attestation.')
+
+    parser_prune.add_argument('--discard', dest='attestations_to_discard', metavar='NOTARYSPEC', action='append',
+                              type=str, default=[],
+                              help='Discard attestations from a specified notary. May be specified multiple times. '
+                                   'Default pending:*.')
+
+    parser_prune.add_argument('timestamp_fd', metavar='TIMESTAMP', type=argparse.FileType('rb'),
+                              help='Existing timestamp; moved to TIMESTAMP.bak')
 
 
     parser_stamp.set_defaults(cmd_func=otsclient.cmds.stamp_command)
     parser_upgrade.set_defaults(cmd_func=otsclient.cmds.upgrade_command)
     parser_verify.set_defaults(cmd_func=otsclient.cmds.verify_command)
     parser_info.set_defaults(cmd_func=otsclient.cmds.info_command)
+    parser_prune.set_defaults(cmd_func=otsclient.cmds.prune_command)
 
     try:
         import git
